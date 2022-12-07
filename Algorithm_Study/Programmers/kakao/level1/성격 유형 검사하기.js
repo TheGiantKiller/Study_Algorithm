ceb6fbd0bc20e460353d287b5c16b344a99f4d75
TheGiantKiller <ktsdsd94@gmail.com>
2022-12-07 14:36:20 +0900

    solve : 성격 유형 검사하기

--- Algorithm_Study/Programmers/kakao/level1/성격 유형 검사하기.js
+++ Algorithm_Study/Programmers/kakao/level1/성격 유형 검사하기.js
@@ -1,87 +1,72 @@
+const calculate_score = (survey, choices, scores) => {
+  let new_survey = survey.split("");
+  let [a_score, b_score] = [0, 0];
+  switch (choices) {
+    case 1:
+      a_score += 3;
+      break;
+    case 2:
+      a_score += 2;
+      break;
+    case 3:
+      a_score += 1;
+      break;
+    case 5:
+      b_score += 1;
+      break;
+    case 6:
+      b_score += 2;
+      break;
+    case 7:
+      b_score += 3;
+      break;
+  }
+  if (a_score > b_score) {
+    scores.set(new_survey[0], scores.get(new_survey[0]) + a_score);
+  } else if (a_score < b_score) {
+    scores.set(new_survey[1], scores.get(new_survey[1]) + b_score);
+  }
+};
+
+const find_answer = (scores) => {
+  let answer = "";
+  const board = [
+    ["R", "T"],
+    ["C", "F"],
+    ["J", "M"],
+    ["A", "N"],
+  ];
+
+  for (let i = 0; i < board.length; i++) {
+    const a_score = scores.get(board[i][0]);
+    const b_score = scores.get(board[i][1]);
+    if (a_score >= b_score) {
+      answer += board[i][0];
+    } else {
+      answer += board[i][1];
+    }
+  }
+
+  return answer;
+};
 function solution(survey, choices) {
-    let answer = ""
-    let boardScore={'R':0,'F':0,'J':0,'N':0,'T':0,'C':0,'M':0,'A':0}
+  var answer = "";
+  let scores = new Map();
+  scores.set("R", 0);
+  scores.set("T", 0);
+  scores.set("C", 0);
+  scores.set("F", 0);
+  scores.set("J", 0);
+  scores.set("M", 0);
+  scores.set("A", 0);
+  scores.set("N", 0);
 
+  for (let i = 0; i < survey.length; i++) {
+    calculate_score(survey[i], choices[i], scores);
+  }
+  answer = find_answer(scores, answer);
 
-    for (let i = 0; i < survey.length; i++){
-        let tmp = calculate(survey[i], choices[i])
-        let alpha = tmp[0][0]
-        let score=tmp[0][1]
-        boardScore[alpha]+=score
-    }
-    // 점수 계산
-
-    answer+=compareScore('R','T',boardScore)
-    answer+=compareScore('C','F',boardScore)
-    answer+=compareScore('J','M',boardScore)
-    answer+=compareScore('A','N',boardScore)
-
-
-    return answer
-
+  return answer;
 }
-const compareScore = (word_a, word_b,boardScore) => {
-    if (boardScore[word_a] > boardScore[word_b]) {
-       return word_a
-    }
-    else if (boardScore[word_a] === boardScore[word_b]) {
-        if (word_a.charCodeAt(0) < word_b.charCodeAt(0)) {
-            return word_a
-        }
-        else {
-            return word_b
-        }
-    }
-    else {
-        return word_b
-    }
-    
-}
-
-
-// 두개중 점수 높은 알파벳 반환
-const calculate = (word,choices) => {
-    let word_a = word[0]
-    let word_b = word[1]
-    
-    let score_a=0
-    let score_b = 0
-    let s=[]
-    let scoreBoard = [3, 2, 1, 0, 1, 2, 3]
-    choices-=1
-    
-    if (choices >= 4) {
-        score_b+=scoreBoard[choices]    
-    }
-    else {
-        score_a+=scoreBoard[choices]
-    }
-
-
-    if (score_a > score_b) {
-        s.push([word_a,score_a])
-        return s    
-    }
-    else if (score_a == score_b) {
-        if (word_a.charCodeAt(0) < word_b.charCodeAt(0)) {
-            s.push([word_a,score_a])
-            return s
-        }
-        else {
-            s.push([word_b,score_b])
-            return s
-        }
-    }
-    else {
-        s.push([word_b,score_b])
-        return s
-    }
-
-    
-
-    
-}
-
-
-solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5])
-solution(["TR", "RT", "TR"],[7, 1, 3])
+solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]);
+solution(["TR", "RT", "TR"], [7, 1, 3]);
