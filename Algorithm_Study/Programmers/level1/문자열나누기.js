3aa7c1b0717916a01117bcba23baf3b4f992e71d
TheGiantKiller <ktsdsd94@gmail.com>
2022-12-17 18:13:38 +0900

    solve : 문자열나누기 level1

--- Algorithm_Study/Programmers/level1/문자열나누기.js
+++ Algorithm_Study/Programmers/level1/문자열나누기.js
@@ -1,29 +1,26 @@
-const checkAnswer = (x_count, other_count) => {
-  return x_count === other_count ? true : false;
-};
 function solution(s) {
-  var answer = 0;
-  let start = 0;
+    var answer = 0;
+    let x=[]
+    let other=0
+    s=s.split('')
 
-  while (start < s.length) {
-    let x = s[start];
-    let [x_count, other_count] = [1, 0];
-    for (let j = start + 1; start < s.length; j++) {
-      let other = s[j];
-      if (checkAnswer(x_count, other_count)) {
-        answer += 1;
-        start = j;
-        break;
+    for(let i=0;i<s.length;i++){
+      let cur=s[i]
+      if(x.length===0 || cur===x[x.length-1]){
+        x.push(cur)
       }
-      if (x === other) {
-        x_count += 1;
-      } else if (x !== other) {
-        other_count += 1;
-      }
+      else{ //다르면
+      other+=1
+      if(x.length===other){
+        x=[]
+        other=0
+        answer+=1
+      }     
     }
   }
-
-  return answer;
-}
-solution("banana");
-solution("abracadabra");
+  return x.length>0?answer+1 : answer
+ }
+solution("banana")
+solution("abaabc")
+solution("abracadabra")
+solution("aaabbaccccabba")
